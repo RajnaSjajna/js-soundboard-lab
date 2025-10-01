cd2697026381c24563f724869ba186be54059681
Rajna <rajne_rajne@yahoo.com>
2025-10-01 09:17:52 +0200

    homework sound

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,12 @@
 
+
+//elements
 const buttons = document.querySelectorAll('.sound-button');
+
 buttons.forEach(button => {
-    button.addEventListener('click', (evt) => {
-        const soundId = evt.target.id;
+    button.addEventListener('click', (e) => {
+        const soundId = e.target.id;
+        
         console.log(soundId);
         const sound = new Audio(`sounds/${soundId}.wav`);
         sound.volume = 0.5;
